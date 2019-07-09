745bf1b292b6d9036b906c257a50e52698e73248
Ben Governali <bgovern93@gmail.com>
2019-07-09 14:46:46 -0400

    initial commit

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { VISIBILITY_FILTERS } from "../constants";
+
 export const getTodoList = store =>
   store && store.todos ? store.todos.allIds : [];
 
@@ -5,6 +7,18 @@ export const getTodoById = (store, id) =>
   store && store.todos && store.todos.byIds
     ? { ...store.todos.byIds[id], id }
     : {};
+
+export const getTodosByVisibilityFilter = (store, visibilityFilter) => {
+  const allTodos = getTodos(store);
+  switch (visibilityFilter) {
+    case VISIBILITY_FILTERS.COMPLETED:
+      return allTodos.filter(todo => todo.completed);
+    case VISIBILITY_FILTERS.INCOMPLETE:
+      return allTodos.filter(todo => !todo.completed);
+    case VISIBILITY_FILTERS.ALL:
+      return allTodos;
+  }
+};
 
 /**
  * example of a slightly more complex selector
